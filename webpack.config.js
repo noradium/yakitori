4e9e75175719d2eb380e99ca2372e5f73c4d93ed
noradium <yusuke_hosono@dwango.co.jp>
2017-08-26 15:56:16 +0900

    safari で fetch が見えてなかったのを修正

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.ts',
+  entry: ['es6-promise', 'whatwg-fetch', './src/main.ts'],
   output: {
     filename: './game/script/main.js',
     libraryTarget: 'commonjs2'
